abd1d82f90c71e9beb3575e3d1d6a79c18824c06
Victor Marciano <victor_marcian0@hotmail.com>
2022-11-20 11:20:24 -0300

    this is the end?

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -19,8 +19,8 @@ typedef struct node
 {
     pf Function;
     int pid;
-    int frequency; // n=0 not repeating | n<0 repeating
-    int priority;  // the grater the more important
+    int frequency;      // n=0 not repeating | n<0 repeating
+    int priority;       // the grater the more important
     int scheduled_time; // the time when the process should occur
     struct node *next;
 } node;
@@ -82,43 +82,80 @@ char *addProcess(node **head, node **tail, pf function, int pid, int frequency, 
     }
 }
 
-pf removeProcess(node **node_to_be_removed, node **head, int* frequency, int* priority)
+pf _removeProcess(node **head, int pid_to_remove, int *frequency, int *priority)
 {
-    //TODO considerar remoção no meio
-    printf("Removing process | PID:%d\n", (*node_to_be_removed)->pid);
+    printf("Removing process | PID:%d\n", pid_to_remove);
+    node *current, *previous = NULL;
+    // free(current);
+    // free(previous);
+    current = *head;
     pf function = NULL;
-    node **next_node, **previous_node, **current_node;
-    free(next_node);
-    free(previous_node);
-    free(current_node);
-    current_node = head;
-    if (*node_to_be_removed == NULL)
+
+    if (head == NULL)
     {
         return NULL;
     }
-    while(1){
-        if ((*current_node)->pid == (*node_to_be_removed)->pid){
-            if((*current_node)->pid == (*head)->pid){
-                head = &((*head)->next);
-            }
-            else{
-                (*previous_node)->next = (*current_node)->next;
-            }
-            break;
-        }
-        //* ERRO AQUI - Não está caindo aqui por algum motivo
-        if((*current_node)->next == NULL){
+
+    while (current->pid != pid_to_remove)
+    {
+        previous = current;
+        if (current->next == NULL)
+        {
             printf("ERROR\nPROCESS NOT FOUND\n");
             exit(-13);
+            break;
         }
-        previous_node = current_node;
-        current_node =  &((*current_node)->next);
+        current = current->next;
     }
-    //next_node = (*node_to_be_removed)->next;
-    function = (*node_to_be_removed)->Function;
-    *frequency = (*node_to_be_removed)->frequency;
-    *priority = (*node_to_be_removed)->priority;
-    //free(*node_to_be_removed);
+
+    function = current->Function;
+    *frequency = current->frequency;
+    *priority = current->priority;
+
+    // check if its the first node
+    if (current == *head)
+    {
+        node *next_node = NULL;
+        next_node = (*head)->next;
+        free(*head);
+        *head = next_node;
+    }
+    else
+    {
+        // check if node is last node
+        if (current->next == NULL)
+        {
+            previous->next = NULL;
+            free(current);
+        }
+        // middle node
+        else
+        {
+            previous->next = current->next;
+            free(current);
+        }
+    }
+    return function;
+}
+
+pf removeProcess(node **head, int *frequency, int *priority)
+{
+    printf("Removing process | PID:%d\n", (*head)->pid);
+    pf function = NULL;
+    node *next_node = NULL;
+
+    if (*head == NULL)
+    {
+        return NULL;
+    }
+
+    next_node = (*head)->next;
+    function = (*head)->Function;
+    *frequency = (*head)->frequency;
+    *priority = (*head)->priority;
+    free(*head);
+    *head = next_node;
+
     return function;
 }
 
@@ -160,44 +197,63 @@ void executeProcess(node **head, node **tail)
     free(current_node);
     current_node = head;
     choosen_node = head;
-    //compare scheduled time for each node
-    while(1){
+    // compare scheduled time for each node
+    while (1)
+    {
         (*current_node)->scheduled_time -= 1;
-        if((*current_node)->scheduled_time <= 0){
-            //* CPU DISPUTE
-            if ((*choosen_node)->priority < (*current_node)->priority && (*choosen_node)->scheduled_time<=0){
+        if ((*current_node)->scheduled_time <= 0)
+        {
+            if ((*choosen_node)->scheduled_time > 0)
+            {
                 choosen_node = current_node;
             }
-            else if((*choosen_node)->scheduled_time * (*choosen_node)->priority < (*current_node)->scheduled_time * (*current_node)->priority){
-                choosen_node = current_node;
+            else
+            {
+                if ((*choosen_node)->priority < (*current_node)->priority && (*choosen_node)->scheduled_time <= 0)
+                {
+                    choosen_node = current_node;
+                }
+                else
+                {
+                    if ((*choosen_node)->priority == (*current_node)->priority)
+                    {
+                        if((*choosen_node)->scheduled_time > (*current_node)->scheduled_time)
+                        {
+                            choosen_node = current_node;
+                        }
+                    }
+                }
             }
         }
-        printf("\ncurrent_node address = %d\n", current_node);
-        if((*current_node)->next != NULL){
+        // printf("\ncurrent_node address = %d\n", current_node);
+        if ((*current_node)->next != NULL)
+        {
             current_node = &((*current_node)->next);
         }
-        else {
+        else
+        {
             break;
         }
     }
     int frequency, priority;
-    if((*choosen_node)->scheduled_time <= 0){
+    if ((*choosen_node)->scheduled_time <= 0)
+    {
+        /*   
         printf("------------------------------------------------------------------------\n");
         printf("choosen_node address : %d\n", choosen_node);
         printf("(*choosen_node)->scheduled_time %d\n", (*choosen_node)->scheduled_time);
         printf("------------------------------------------------------------------------\n");
-        pf function = removeProcess(choosen_node, head, &frequency, &priority);
+        */
+        pf function = _removeProcess(head, (*choosen_node)->pid, &frequency, &priority);
         if (function != NULL)
         {
-            /*
-                reschedule = function();
-                if (strcmp(reschedule, "REPEAT") == 0)
-                {
-                    printf("FREQUENCY %i\tPRIORITY %i\n", frequency, priority);
-                    printf("Rescheduling Process\n");
-                    addProcess(head, tail, function, rand() % 100, frequency, priority, (*choosen_node)->frequency);
-                }
-            */
+            reschedule = function();
+            if (strcmp(reschedule, "REPEAT") == 0)
+            {
+                //printf("FREQUENCY %i\tPRIORITY %i\n", frequency, priority);
+                //printf("Rescheduling Process\n");
+                addProcess(head, tail, function, rand() % 100, frequency, priority, (*choosen_node)->frequency);
+            }
         }
         printf("------------------------------------\n\n");
     }
@@ -215,18 +271,18 @@ int main()
     node *tail = NULL;
 
     // head = (node *) malloc(sizeof(node));
-    printf("Add Process: %s\n\n", addInitialProcess(&head, &tail, function1, 1, 5, 1, 1));
-    printf("Add Process: %s\n\n", addProcess(&head, &tail, function2, 2, 2, 2, 2));
+    printf("Add Process: %s\n\n", addInitialProcess(&head, &tail, function1, 1, 5, 1, 2));
+    printf("Add Process: %s\n\n", addProcess(&head, &tail, function2, 2, 2, 2, 1));
     printf("Add Process: %s\n\n", addProcess(&head, &tail, function3, 3, 3, 4, 2));
-    //printf("Add Process: %s\n\n", addProcess(&head, &tail, function4, 4, 5, 1, 6));
-    //printf("Add Process: %s\n\n", addProcess(&head, &tail, function5, 5, 5, 2, 5));
+    printf("Add Process: %s\n\n", addProcess(&head, &tail, function4, 4, 5, 1, 6));
+    // printf("Add Process: %s\n\n", addProcess(&head, &tail, function5, 5, 5, 2, 5));
 
     int pid = getBufferSize(&head);
     while (counter >= 0)
     {
         if (head->next != NULL)
         {
-            printf("\ncounter\t%d",counter);
+            printf("\ncounter\t%d\n", counter);
             executeProcess(&head, &tail);
         }
         else
@@ -240,5 +296,5 @@ int main()
         }
     }
 
-    printf("##### Kernel session closing ##### \n");
+    printf("\n##### Kernel session closing ##### \n");
 }
